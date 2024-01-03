3a5b6930ed39c74ed70056f7e7c377413bf4b6d7
Lea Robinson <Lea.robinson.87@gmail.com>
2024-01-03 17:13:23 +0100

    Added basic movement

---
+++ Scenes/Entities/Player/STATES.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var JUMP = $JUMP
+@onready var MOVE = $MOVE
+@onready var IDLE = $IDLE
+@onready var FALL = $FALL
+
+func enter_state():
+	pass
+
+func exit_state():
+	pass
+
+func update(delta):
+	return null
+
+
---
+++ Scenes/Entities/Player/move.gd
@@ -0,0 +1,12 @@
+extends "state.gd"
+
+func update(delta):
+	Player.gravity(delta)
+	player_movement()
+	if Player.velocity.x == 0:
+		return STATES.IDLE
+	if Player.velocity.y > 0:
+		return STATES.FALL
+	if Player.jump_input:
+		return STATES.JUMP
+	return null
