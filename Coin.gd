10adca4f43e8e6a025fec9edb6148e7ee98bb8fb
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2024-01-08 11:26:13 +0100

    Coin with collision and animations

---
+++ Coin.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var anim= $AnimatedSprite2D
+
+func _ready():
+	anim.play("Coin_spin")
+
+func _process(delta):
+	pass
+
+
+func _on_coin_body_entered(body):
+	#queue_free()
+	$AnimationPlayer.play("coin_bounce")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	queue_free()
