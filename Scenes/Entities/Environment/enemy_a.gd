15eac6cd8ec6e3b2399cbdf4e24de4a689e93ae7
MoaOrLess <82501535+MoaOrLess@users.noreply.github.com>
2024-01-11 18:45:24 +0100

    enemy animations and slightly better enemy code

--- Scenes/Entities/Environment/enemy_a.gd
+++ Scenes/Entities/Environment/enemy_a.gd
@@ -11,20 +11,28 @@ var body
 
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity_value * speed * delta 
 	if player_chase == true:
 		position.x += (player.position.x - position.x)/ speed
 		move_and_slide()
-	if not is_on_floor():
-		velocity.y += gravity_value * speed * delta 
+		if (player.position.x - position.x) > 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+
+
 
 
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
+	$AnimatedSprite2D.play("Gel_Move")
 
 
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+	$AnimatedSprite2D.play("Gel_Idle")
 
 
